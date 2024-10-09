06d8f03e55da12718a67b68ee3a0efb51c45829b
taunojokinen <128710496+taunojokinen@users.noreply.github.com>
2024-10-09 16:55:10 +0300

    lukee ja kirjoittaa

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection,addDoc,serverTimestamp, query, onSnapshot } from "firebase/firestore";
-//import {auth} from './Constants.js';
+
 
 
 // TODO: Add SDKs for Firebase products that you want to use
